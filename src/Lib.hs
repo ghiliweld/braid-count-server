1a42592a364f52147d6bd420828e13e124bee92c
ghiliweld <ghiliaweld@gmail.com>
2021-01-04 11:37:37 -0500

    got mvar stuff to work

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( server
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Network.Wai.Handler.Warp (run)  
+import Network.Wai
+import Network.HTTP.Types.Status (status200)
+import Network.HTTP.Types.Method                        (Method, methodGet, methodPut, methodPatch)
+import Data.Monoid (mconcat)
+import qualified Data.Text as T
+import Data.ByteString.Lazy ( ByteString )
+import qualified Data.Text.Internal.Lazy as L
+import Control.Concurrent.MVar
+    ( swapMVar, newMVar, readMVar, MVar )
+
+var :: IO (MVar ByteString)
+var = newMVar "yo\n"
+
+application :: MVar ByteString -> Application
+application v req respond = 
+    if requestMethod req == methodGet
+    then readMVar v >>= \b -> respond $ responseLBS status200 [("Content-Type", "text/plain")] b
+    else strictRequestBody req >>= \body -> swapMVar v body >> readMVar v >>= \b -> respond $ responseLBS status200 [("Content-Type", "text/plain")] b
+
+server :: IO ()
+server = var >>= \v -> run 3000 $ application v
